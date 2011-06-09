51f9e67d3dba8fab40f65722827b35531d146cfa
Marian Theisen <marian@cice-online.net>
2011-06-09 12:36:09 +0200

    create keyword uuids

--- lib/aperture_ar/keyword.rb
+++ lib/aperture_ar/keyword.rb
@@ -1,8 +1,26 @@
+require 'digest'
+
 module ApertureAr
   class Keyword < Base
     establish_connection :library
     
     has_many :keyword_for_versions, :foreign_key => 'keywordId'
     has_many :versions, :through => :keyword_for_versions
+    
+    before_create :create_uuid
+    
+    protected
+    def create_uuid
+      self.uuid = sha1 name, Time.now.to_i.to_s
+    end
+    
+    def sha1 *strings
+      d = Digest::SHA1.new
+      strings.each do |string|
+        d << string
+      end
+      
+      d.to_s
+    end
   end
 end
